0b88ebe9ad1c75d2962d691f194e41c8c7efb4da
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-19 14:04:26 +0700

    initial commit

---
+++ src/main/java/com/haraif/real_time_chat_application/RealTimeChatApplication.java
@@ -0,0 +1,13 @@
+package com.haraif.real_time_chat_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haraif/real_time_chat_application/RealTimeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haraif.real_time_chat_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
