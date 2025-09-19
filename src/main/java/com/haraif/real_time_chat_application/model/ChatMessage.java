685272e79616abb2241a792b4b102c47664880b2
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-19 14:50:29 +0700

    Add JPA entity annotations to ChatMessage model
    Annotated ChatMessage with JPA and validation annotations, added fields for sender, receiver, content, timestamp, type, and roomId, and defined MessageType enum. This prepares the model for persistence and validation in the chat application.

--- src/main/java/com/haraif/real_time_chat_application/model/ChatMessage.java
+++ src/main/java/com/haraif/real_time_chat_application/model/ChatMessage.java
@@ -1,5 +1,46 @@
 package com.haraif.real_time_chat_application.model;
 
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "chat_message")
+@Data
+@Builder
 public class ChatMessage {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+
+  private String sender;
+  private String receiver;
+
+  @Size(max = 2000)
+  private String content;
+
+  private Instant timestamp;
+
+  @Enumerated(EnumType.STRING)
+  private MessageType type;
+
+  private String roomId;
+
+  public enum MessageType {
+    CHAT, JOIN, LEAVE, PRIVATE
+  }
 }
