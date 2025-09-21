300afae597583911c972be541c0a23e04173dfab
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-21 14:16:27 +0700

    Add AppUser entity for user management in the application

---
+++ src/main/java/com/haraif/real_time_chat_application/model/AppUser.java
@@ -0,0 +1,29 @@
+package com.haraif.real_time_chat_application.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Table(name = "app_user")
+public class AppUser {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+
+  @Column(unique = true, nullable = false)
+  private String username;
+
+}
