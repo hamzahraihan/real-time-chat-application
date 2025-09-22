347954f23c86cb0b63d029096e6eacff79ba67d3
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-22 09:17:36 +0700

    Add @Repository annotation to AppUserRepository interface

--- src/main/java/com/haraif/real_time_chat_application/repository/AppUserRepository.java
+++ src/main/java/com/haraif/real_time_chat_application/repository/AppUserRepository.java
@@ -3,9 +3,11 @@ package com.haraif.real_time_chat_application.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.haraif.real_time_chat_application.model.AppUser;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, String> {
   Optional<AppUser> findByUsername(String username);
 }
