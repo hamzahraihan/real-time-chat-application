fb1638d69872187e2d30215f6e518fb99f37454a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-22 09:17:45 +0700

    Refactor ChatRoomRepository to remove unused methods and add findByName for chat room retrieval

--- src/main/java/com/haraif/real_time_chat_application/repository/ChatRoomRepository.java
+++ src/main/java/com/haraif/real_time_chat_application/repository/ChatRoomRepository.java
@@ -1,16 +1,13 @@
 package com.haraif.real_time_chat_application.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.haraif.real_time_chat_application.model.ChatMessage;
 import com.haraif.real_time_chat_application.model.ChatRoom;
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, String> {
-  List<ChatMessage> findRoomIdOrderByTimestampAsc(String roomId);
-
-  List<ChatMessage> findBySenderAndReceiverOrderByTimestampAsc(String sender, String receiver);
+  Optional<ChatRoom> findByName(String name);
 }
