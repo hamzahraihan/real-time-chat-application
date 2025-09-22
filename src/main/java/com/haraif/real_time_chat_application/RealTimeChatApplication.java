8c677779dfaa3c907a5ff1a79890724b9421a573
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-22 09:17:23 +0700

    Refactor application configuration by adding EnableAutoConfiguration and ComponentScan annotations

--- src/main/java/com/haraif/real_time_chat_application/RealTimeChatApplication.java
+++ src/main/java/com/haraif/real_time_chat_application/RealTimeChatApplication.java
@@ -1,9 +1,13 @@
 package com.haraif.real_time_chat_application;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = { "com.haraif.real_time_chat_application" })
 public class RealTimeChatApplication {
 
 	public static void main(String[] args) {
