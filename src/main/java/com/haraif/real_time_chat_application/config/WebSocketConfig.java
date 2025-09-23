749c99536663f441b5f653a8808d12624a0b830a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-23 14:40:58 +0700

    Enhance WebSocket configuration by adding UserHandshakeInterceptor and adjusting order

--- src/main/java/com/haraif/real_time_chat_application/config/WebSocketConfig.java
+++ src/main/java/com/haraif/real_time_chat_application/config/WebSocketConfig.java
@@ -1,18 +1,24 @@
 package com.haraif.real_time_chat_application.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.haraif.real_time_chat_application.interceptor.UserHandshakeInterceptor;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
+				.addInterceptors(new UserHandshakeInterceptor())
 				.setAllowedOriginPatterns("http://localhost:5173", "http://localhost:3000")
 				.withSockJS();
 
