06a1b4e05055a744d38540601c8fb50de15cfd64
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-24 14:15:47 +0700

    Remove unused import for RequestParam in ChatController

--- src/main/java/com/haraif/real_time_chat_application/controller/ChatController.java
+++ src/main/java/com/haraif/real_time_chat_application/controller/ChatController.java
@@ -15,7 +15,6 @@ import com.haraif.real_time_chat_application.service.ChatService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
