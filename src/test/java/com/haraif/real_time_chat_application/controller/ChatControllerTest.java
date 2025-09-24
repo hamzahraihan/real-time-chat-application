a4c30399f2458ec24f71523edda25919e5ccf78d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-24 14:16:05 +0700

    Add ChatControllerTest class for testing chat message handling

---
+++ src/test/java/com/haraif/real_time_chat_application/controller/ChatControllerTest.java
@@ -0,0 +1,62 @@
+package com.haraif.real_time_chat_application.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.haraif.real_time_chat_application.dto.ChatMessageDTO;
+import com.haraif.real_time_chat_application.model.ChatMessage;
+import com.haraif.real_time_chat_application.repository.ChatMessageRepository;
+import com.haraif.real_time_chat_application.service.ChatService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+public class ChatControllerTest {
+
+	@Mock
+	private SimpMessagingTemplate template;
+
+	@Mock
+	private ChatMessageRepository chatMessageRepository;
+
+	@InjectMocks
+	private ChatService chatService;
+
+	@Test
+	void testHandleRoomMessage() {
+		ChatMessageDTO dto = new ChatMessageDTO();
+		dto.setSender("alice");
+		dto.setContent("hello from test");
+
+		ChatMessage msg = chatService.handleRoomMessage(dto, "room1");
+
+		verify(chatMessageRepository).save(any(ChatMessage.class));
+		verify(template).convertAndSend(eq("/topic/room.room1"), any(ChatMessage.class));
+
+	}
+
+	@Test
+	void testHandlePrivateMessage() {
+		ChatMessageDTO dto = new ChatMessageDTO();
+		dto.setSender("alice");
+		dto.setContent("hello to johnny");
+		dto.setReceiver("johnny");
+
+		ChatMessage msg = chatService.handlePrivateMessage(dto);
+
+		verify(chatMessageRepository).save(any(ChatMessage.class));
+		verify(template).convertAndSendToUser(eq(dto.getReceiver()), eq("/queue/messages"), any(ChatMessage.class));
+	}
+}
