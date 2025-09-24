4c9c3f35f59308c871013331eddf2df60ef88898
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-24 14:15:57 +0700

    Refactor ChatService to add missing @Transactional annotations and improve logging format

--- src/main/java/com/haraif/real_time_chat_application/service/ChatService.java
+++ src/main/java/com/haraif/real_time_chat_application/service/ChatService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.haraif.real_time_chat_application.dto.ChatMessageDTO;
 import com.haraif.real_time_chat_application.model.ChatMessage;
@@ -27,6 +28,7 @@ public class ChatService {
 	@Autowired
 	private ChatRoomRepository chatRoomRepository;
 
+	@Transactional
 	public ChatMessage handleRoomMessage(ChatMessageDTO dto, String roomId) {
 		ChatMessage msg = ChatMessage.builder()
 				.sender(dto.getSender())
@@ -37,27 +39,32 @@ public class ChatService {
 				.build();
 
 		chatMessageRepository.save(msg);
-		messagingTemplate.convertAndSend("/topic/room" + roomId, msg);
+		messagingTemplate.convertAndSend("/topic/room." + roomId, msg);
 		log.info("[Room {}] {}: {}", roomId, msg.getSender(), msg.getContent());
 		return msg;
 	}
 
+	@Transactional
 	public ChatMessage handlePrivateMessage(ChatMessageDTO dto) {
 		ChatMessage msg = ChatMessage.builder()
 				.sender(dto.getSender())
 				.content(dto.getContent())
+				.receiver(dto.getReceiver())
 				.timestamp(Instant.now())
 				.type(ChatMessage.MessageType.PRIVATE)
 				.build();
 
 		chatMessageRepository.save(msg);
 		messagingTemplate.convertAndSendToUser(dto.getReceiver(), "/queue/messages", msg);
-		log.info("[private {} -> {}] {}: {}", msg.getSender(), msg.getReceiver(), msg.getContent());
+		log.info("[private] {} -> {}: {}", msg.getSender(), msg.getReceiver(), msg.getContent());
 		return msg;
 	}
 
 	// helper: fetch recent message for a room
+	@Transactional(readOnly = true)
 	public List<ChatMessage> getRoomHistory(String roomId) {
-		return chatMessageRepository.findByRoomIdOrderByTimestampAsc(roomId);
+		List<ChatMessage> chatMessage = chatMessageRepository.findByRoomIdOrderByTimestampAsc(roomId);
+
+		return chatMessage;
 	}
 }
