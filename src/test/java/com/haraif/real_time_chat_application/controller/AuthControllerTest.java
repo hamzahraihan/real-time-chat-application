d2acfea66deba26478412db8bf1ba78a26e2bffc
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-24 14:16:01 +0700

    Add initial AuthControllerTest class for testing authentication controller

---
+++ src/test/java/com/haraif/real_time_chat_application/controller/AuthControllerTest.java
@@ -0,0 +1,10 @@
+package com.haraif.real_time_chat_application.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthControllerTest {
+
+}
