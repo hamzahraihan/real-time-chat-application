07a84ad956025debae43fb138f68f176ece13f77
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-02 10:26:38 +0700

    Remove unused import for GeneratedValue in ChatMessage model

--- src/main/java/com/haraif/real_time_chat_application/model/ChatMessage.java
+++ src/main/java/com/haraif/real_time_chat_application/model/ChatMessage.java
@@ -5,7 +5,6 @@ import java.time.Instant;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Size;
