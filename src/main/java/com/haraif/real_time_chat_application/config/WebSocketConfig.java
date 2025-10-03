69b3c31989ca21fc10388157f22242ac5042dba1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-03 13:26:45 +0700

    Update WebSocketConfig.java

--- src/main/java/com/haraif/real_time_chat_application/config/WebSocketConfig.java
+++ src/main/java/com/haraif/real_time_chat_application/config/WebSocketConfig.java
@@ -1,12 +1,19 @@
 package com.haraif.real_time_chat_application.config;
 
+import java.security.Principal;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 import com.haraif.real_time_chat_application.interceptor.UserHandshakeInterceptor;
 
@@ -19,30 +26,28 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
 				.addInterceptors(new UserHandshakeInterceptor())
-				.setAllowedOriginPatterns("http://localhost:5173", "http://localhost:3000")
+				.setHandshakeHandler(handshakeHandler())
+				.setAllowedOriginPatterns("*")
 				.withSockJS();
-
 	}
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/topic");
+		registry.enableSimpleBroker("/topic", "/queue");
 		registry.setApplicationDestinationPrefixes("/app");
-		// registry.setUserDestinationPrefix("/user");
+		registry.setUserDestinationPrefix("/user");
 	}
 
-	// @Bean
-	// DefaultHandshakeHandler handshakeHandler() {
-	// return new DefaultHandshakeHandler() {
-	// @Override
-	// protected Principal determineUser(ServerHttpRequest request, WebSocketHandler
-	// wsHandler,
-	// Map<String, Object> attributes) {
-	// // For demo: use a query param ?username=alice
-	// String username = request.getURI().getQuery().replace("username=", "");
-	// return () -> username;
-	// }
-	// };
-	// }
+	@Bean
+	DefaultHandshakeHandler handshakeHandler() {
+		return new DefaultHandshakeHandler() {
+			@Override
+			protected Principal determineUser(ServerHttpRequest request,
+					WebSocketHandler wsHandler,
+					Map<String, Object> attributes) {
+				return (Principal) attributes.get("principal"); // ✅ return your StompPrincipal
+			}
+		};
+	}
 
 }
