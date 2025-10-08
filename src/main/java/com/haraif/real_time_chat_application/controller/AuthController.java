84717868fe23cdc9de5bddb3d7164356e63afb48
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-08 14:16:37 +0700

    Implement registration endpoint in AuthController

--- src/main/java/com/haraif/real_time_chat_application/controller/AuthController.java
+++ src/main/java/com/haraif/real_time_chat_application/controller/AuthController.java
@@ -1,5 +1,27 @@
 package com.haraif.real_time_chat_application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.haraif.real_time_chat_application.dto.AuthRequestDTO;
+import com.haraif.real_time_chat_application.dto.AuthResponseDTO;
+import com.haraif.real_time_chat_application.service.AuthService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+  @Autowired
+  private AuthService authService;
+
+  @PostMapping("/register")
+  public ResponseEntity<AuthResponseDTO> register(@RequestBody AuthRequestDTO request) {
+    return authService.register(request);
+  }
 
 }
