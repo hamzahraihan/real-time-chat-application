96cea42e273e76b6dd95b4fc5e8898c07f5490f7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-08 14:16:46 +0700

    Add AuthResponseDTO for authentication responses

---
+++ src/main/java/com/haraif/real_time_chat_application/dto/AuthResponseDTO.java
@@ -0,0 +1,16 @@
+package com.haraif.real_time_chat_application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponseDTO {
+  private String token;
+  private String message;
+  private String error;
+}
