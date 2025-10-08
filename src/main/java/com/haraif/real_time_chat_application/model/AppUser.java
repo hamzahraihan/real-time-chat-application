0c2c1ca682347bab4405709725d6db2d46e35af7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-08 14:16:59 +0700

    Remove unused imports and clean up AppUser entity definition

--- src/main/java/com/haraif/real_time_chat_application/model/AppUser.java
+++ src/main/java/com/haraif/real_time_chat_application/model/AppUser.java
@@ -2,8 +2,6 @@ package com.haraif.real_time_chat_application.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,10 +18,12 @@ import lombok.NoArgsConstructor;
 public class AppUser {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private String id;
 
   @Column(unique = true, nullable = false)
   private String username;
+
+  @Column(nullable = false)
+  private String password;
 
 }
