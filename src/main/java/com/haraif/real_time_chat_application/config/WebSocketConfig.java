61b74575bb84f95b25ec12a962b162291af0766a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-10-14 11:22:20 +0700

    Refactor WebSocketConfig to use dependency injection for UserHandshakeInterceptor and clean up unused imports

--- src/main/java/com/haraif/real_time_chat_application/config/WebSocketConfig.java
+++ src/main/java/com/haraif/real_time_chat_application/config/WebSocketConfig.java
@@ -1,19 +1,13 @@
 package com.haraif.real_time_chat_application.config;
 
-import java.security.Principal;
-import java.util.Map;
-
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 import com.haraif.real_time_chat_application.interceptor.UserHandshakeInterceptor;
 
@@ -22,13 +16,15 @@ import com.haraif.real_time_chat_application.interceptor.UserHandshakeIntercepto
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+	@Autowired
+	private UserHandshakeInterceptor userHandshakeInterceptor;
+
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/ws")
-				.addInterceptors(new UserHandshakeInterceptor())
-				.setHandshakeHandler(handshakeHandler())
-				.setAllowedOriginPatterns("*")
-				.withSockJS();
+				.addInterceptors(userHandshakeInterceptor)
+				// .setHandshakeHandler(handshakeHandler())
+				.setAllowedOriginPatterns("*");
 	}
 
 	@Override
@@ -36,18 +32,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 		registry.enableSimpleBroker("/topic", "/queue");
 		registry.setApplicationDestinationPrefixes("/app");
 		registry.setUserDestinationPrefix("/user");
-	}
-
-	@Bean
-	DefaultHandshakeHandler handshakeHandler() {
-		return new DefaultHandshakeHandler() {
-			@Override
-			protected Principal determineUser(ServerHttpRequest request,
-					WebSocketHandler wsHandler,
-					Map<String, Object> attributes) {
-				return (Principal) attributes.get("principal");
-			}
-		};
 	}
 
 }
